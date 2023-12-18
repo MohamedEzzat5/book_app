177bbbf2bf789a28fa6dedd885da1850b207bfdf
Mohamed Ezzat <MohamedEzzat5>
2023-12-18 06:22:42 +0200

    .

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,5 +1,5 @@
 import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
-import 'package:book_app/core/errors/faiures.dart';
+import 'package:book_app/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo
--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,6 +1,6 @@
 import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:book_app/Features/home/data/repos/home_repo.dart';
-import 'package:book_app/core/errors/faiures.dart';
+import 'package:book_app/core/errors/failures.dart';
 import 'package:book_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
